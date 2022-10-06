bf765b5f89a06984d4c9a7c4a95c5a4fe76d6071
linxside <39219399+linxside@users.noreply.github.com>
2022-10-06 11:47:10 +0200

    chore: upgrade prometheus client library (#132)
    * chore: upgrade prometheus client library
    
    * lint: fix imports

--- pkg/microservice/metrics/metrics.go
+++ pkg/microservice/metrics/metrics.go
@@ -5,14 +5,23 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 // StartMetricsExporter is blocking.
 func StartMetricsExporter(address string) error {
+	prometheus.Unregister(collectors.NewGoCollector())
+	prometheus.MustRegister(collectors.NewGoCollector(collectors.WithGoCollectorRuntimeMetrics(collectors.MetricsAll)))
+	prometheus.MustRegister(collectors.NewBuildInfoCollector())
+
 	router := mux.NewRouter()
 	router.StrictSlash(true)
-	router.Handle("/metrics", promhttp.Handler())
+	router.Handle("/metrics", promhttp.HandlerFor(
+		prometheus.DefaultGatherer, promhttp.HandlerOpts{
+			EnableOpenMetrics: true,
+		}))
 
 	server := &http.Server{
 		Addr:              address,
